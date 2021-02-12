4907327f497651c1eb4f46430395b902f1069370
Rem Kim <kim.rmi.93@gmail.com>
2021-02-12 11:42:29 -0500

    splash page
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,57 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import classNames from 'classnames';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Developer | Wonolo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main} >
-        <span style={{backgroundColor: '#FFDC00'}} className="p-4 rounded-md">
-        <h1 className="text-6xl text-black">
-          <a href="https://developer.wonolo.com">Developer Wonolo</a>
-        </h1>
-      </span>
+      <header className="w-full flex justify-start p-8">
+          <Image
+            src="/wonolo-logo-original.svg"
+            height={23}
+            width={100}
+            alt="Wonolo Tech"
+            priority
+          />
+          <span className="inline ml-2 text-2xl">Developer Center</span>
+          <span />
+      </header>
+      <main>
+      <div className={classNames(styles.patternbottom, 'w-full', 'sm:w-1/3')} />
+      <div className={classNames(styles.patterntop, 'hidden', 'md:block', 'sm:w-1/3')} />
+        <section role="hero" className={styles.container}>
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl text-center">
+            <span className="block xl:inline">
+              The Staffing infrastructure for
+            </span>{' '}
+            <span className="block xl:inline text-yellow-400">
+              your business
+            </span>
+          </h1>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-3xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 text-center">
+            Thousands of companies use Wonolo's software and APIs to schedule,
+            manage and pay their on-demand workforce
+          </p>
+          <div class="rounded-md shadow mt-16">
+            <a
+              href="mailto:requestapiaccess+noreply@wonolo.com"
+              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-400 hover:bg-yellow-500 md:py-4 md:text-lg md:px-10"
+            >
+              Request Access
+            </a>
+          </div>
+        </section>
       </main>
       <footer className={styles.footer}>
-        <a
-        href="https://wonolo.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wonolo
+        <a href="https://wonolo.com" target="_blank" rel="noopener noreferrer">
+          © Wonolo, Inc
         </a>
       </footer>
     </div>
-  )
+  );
 }
